8625bfc053b747d86dc640dced4e2a104e333d90
Giannis Damilatis <johndamilatis786@gmail.com>
2023-05-03 10:48:07 +0300

    configure config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DEBUG = True
-DATABASE_URI = "mongodb://localhost:27017/"
-DATABASE_NAME = "mydb"
+DEBUG = False
+DATABASE_URI = "mongodb+srv://mrjohnny786:kalamata1a@patientsdata.c79pi.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = "searchApp"
 COLLECTION_NAME = "garments"

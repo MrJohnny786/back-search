12e21ed8bb827461b325a79a96818cb791cdfb7b
Giannis Damilatis <johndamilatis786@gmail.com>
2023-05-20 15:28:38 +0300

    app for deployment

--- api/app.py
+++ api/app.py
@@ -1,18 +1,18 @@
 import sys
 import re
 
-sys.path.append("/helpers")
 
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
-from methods import (
-    toStrings,
-    sanitizeName,
-    sanitizePageAndLimit,
-    convert_to_dict,
-)
 
-# from flask_cors import CORS
+# from methods import (
+#     toStrings,
+#     sanitizeName,
+#     sanitizePageAndLimit,
+#     convert_to_dict,
+# )
+
+from flask_cors import CORS
 import os
 
 port = int(os.environ.get("PORT", 5000))
@@ -22,8 +22,55 @@ port = int(os.environ.get("PORT", 5000))
 
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 app.config.from_pyfile("config.py")
+
+from bson import ObjectId
+import re
+
+
+def toStrings(data):
+    if data:
+        for document in data:
+            # Check if the document has an _id field and if it's an ObjectId
+            if "_id" in document and isinstance(document["_id"], ObjectId):
+                # Convert the ObjectId to a string
+                document["_id"] = str(document["_id"])
+        return data
+    else:
+        return None
+
+
+def sanitizeName(name):
+    if name:
+        name = re.sub(r"[^\w\s-]", "", name)
+        if name:
+            return name
+        else:
+            return None
+    else:
+        return None
+
+
+def sanitizePageAndLimit(page=1, limit=10):
+    try:
+        page = int(page)
+        limit = int(limit)
+    except ValueError:
+        page = 1
+        limit = 10
+    if page < 1:
+        page = 1
+    if limit < 1 or limit > 1000:
+        limit = 10
+
+    return page, limit
+
+
+def convert_to_dict(data):
+    # Convert the data to a list of dictionaries
+    data = [document for document in data]
+    return data
 
 
 try:
